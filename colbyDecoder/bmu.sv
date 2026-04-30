4186e00e0f0e8e1820bbe39af4fd3713d7d41225
seriousColb <cmar14.cm@gmail.com>
2026-04-30 11:33:45 -0400

    Parameterize bmu module and improve encoding logic
    Refactor bmu module to parameterize curr_state and update encoding logic.

--- colbyDecoder/bmu.sv
+++ colbyDecoder/bmu.sv
@@ -20,57 +20,64 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module bmu(
-    input logic [1:0]rx,
-    input logic [1:0]curr_state,
-    output logic [1:0]branch_weight0,
-    output logic [1:0]branch_weight1
+module bmu #(parameter K =3)(
+    input logic [1:0] rx,
+    input logic [K-2:0] curr_state,
+    output logic [1:0] branch_weight0,
+    output logic [1:0] branch_weight1
     );
-
-    logic [1:0]enc0; //expected encoder output for lower branch
-    logic [1:0]enc1; //expected encoder output for upper branch
-
-    logic [1:0]hamming_weight0;
-    logic [1:0]hamming_weight1;
-
-    // Determine expected encoder outputs based on current state
+    
+    logic [1:0] enc0; //expected encoder output for lower branch
+    logic [1:0] enc1; //expected encoder output for upper branch
+    
+    logic [K-1:0]reg0;
+    logic [K-1:0]reg1;
+    
     always_comb begin
-        // No inferred latches here...
+        //No inferred latches
         enc0 = 2'b00;
         enc1 = 2'b00;
-
-        case (curr_state)
-            2'b00: begin enc0 = 2'b00; enc1 = 2'b11;/*
-                branch_weight0 = (rx[0] ^ enc0[0]) + (rx[1] ^ enc0[1]);
-
-                branch_weight1 = (rx[0] ^ enc1[0]) + (rx[1] ^ enc1[1]);*/
+        //Cancatenate possible inputs on curr_state
+        reg0 = {1'b0, curr_state};
+        reg1 = {1'b1, curr_state};
+        //Calculate next possible encoded states
+        case(K)
+            7: begin
+            //if 0 is received
+                enc0[0] = reg0[6] ^ reg0[4] ^ reg0[3] ^ reg0[1] ^ reg0[0];
+                enc0[1] = reg0[6] ^ reg0[5] ^ reg0[4] ^ reg0[3] ^ reg0[0];
+            //if 1 is received
+                enc1[0] = reg1[6]^reg1[4]^reg1[3]^reg1[1]^reg1[0];
+                enc1[1] = reg1[6]^reg1[5]^reg1[4]^reg1[3]^reg1[0];
             end
-            2'b10: begin enc0 = 2'b11; enc1 = 2'b00; /*
-                branch_weight0 = (rx[0] ^ enc0[0]) + (rx[1] ^ enc0[1]);
-
-                branch_weight1 = (rx[0] ^ enc1[0]) + (rx[1] ^ enc1[1]);*/
+            5: begin
+            //if 0 is received
+                enc0[0] = reg0[4] ^ reg0[3] ^ reg0[1] ^ reg0[0];
+                enc0[1] = reg0[4] ^ reg0[3] ^ reg0[2] ^ reg0[1] ^ reg0[0];
+            //if 1 is received
+                enc1[0] = reg1[4] ^ reg1[3] ^ reg1[1] ^ reg1[0];
+                enc1[1] = reg1[4] ^ reg1[3] ^ reg1[2] ^ reg1[1] ^ reg1[0];
             end
-            2'b01: begin enc0 = 2'b10; enc1 = 2'b01; /*
-                branch_weight0 = (rx[0] ^ enc0[0]) + (rx[1] ^ enc0[1]);
-
-                branch_weight1 = (rx[0] ^ enc1[0]) + (rx[1] ^ enc1[1]);*/
+            4: begin
+            //if 0 is received
+                enc0[0] = reg0[3] ^ reg0[1] ^ reg0[0];
+                enc0[1] = reg0[3] ^ reg0[2] ^ reg0[1] ^ reg0[0];
+            //if 1 is received
+                enc1[0] = reg1[3] ^ reg1[1] ^ reg1[0];
+                enc1[1] = reg1[3] ^ reg1[2] ^ reg1[1] ^ reg1[0];
             end
-            2'b11: begin enc0 = 2'b01; enc1 = 2'b10; /*
-                branch_weight0 = (rx[0] ^ enc0[0]) + (rx[1] ^ enc0[1]);
-
-                branch_weight1 = (rx[0] ^ enc1[0]) + (rx[1] ^ enc1[1]);*/
+            default begin
+            //if 0 is received
+                enc0[0] = reg0[2] ^ reg0[0]; 
+                enc0[1] = reg0[2] ^ reg0[1] ^ reg0[0];
+            //if 1 is received
+                enc1[0] = reg1[2] ^ reg1[0]; 
+                enc1[1] = reg1[2] ^ reg1[1] ^ reg1[0];
             end
         endcase
-    end
-
-    // Hamming distance
+        //Calculate branch weights        
+        branch_weight0 = (rx[0]^enc0[0]) + (rx[1]^enc0[1]);
+        branch_weight1 = (rx[0]^enc1[0]) + (rx[1]^enc1[1]);        
     
-    always_comb begin
-        branch_weight0 =
-            (rx[0] ^ enc0[0]) + (rx[1] ^ enc0[1]);
-
-        branch_weight1 =
-            (rx[0] ^ enc1[0]) + (rx[1] ^ enc1[1]);
     end
-    
 endmodule
