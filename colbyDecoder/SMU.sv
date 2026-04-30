e1d30c68e988922c30a457524d6dd78497a2f258
seriousColb <cmar14.cm@gmail.com>
2026-04-30 11:32:48 -0400

    Refactor SMU module for parameterized inputs and states

--- colbyDecoder/SMU.sv
+++ colbyDecoder/SMU.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Colby J. Martin
 // 
 // Create Date: 12/14/2025 11:20:49 AM
 // Design Name: 
@@ -20,20 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module SMU #(
-    parameter int TB_DEPTH = 16
-)(
+module SMU #(parameter TB_DEPTH = 16, K = 3)(
     input  logic        clk,
     input  logic        rst,
 
     // Write interface
     input  logic        write_en,
-    input  logic        sel_in [0:3],
+    input  logic        sel_in[0:(2**(K-1))-1],
     input  logic        index,
 
     // Traceback control
     input  logic        tb_start,
-    input  logic [1:0]  tb_state_start,
+    input  logic [K-2:0]  tb_state_start,
 
     // Traceback output
     output logic        tb_bit,
@@ -41,86 +39,151 @@ module SMU #(
     output logic        tb_active
 );
 
+    //States for FSM
+    typedef enum logic [2:0] {
+    START_SMU,
+    SMU_IDLE,
+    WRITE,
+    TRACEBACK
+    } smu_state_t;
     // survivor_mem[time][state]
-    logic [3:0] survivor_mem [0:TB_DEPTH-1];
+    logic [(2**(K-1))-1:0] survivor_mem [0:TB_DEPTH-1];
     logic [$clog2(TB_DEPTH):0] tb_count;
     logic [$clog2(TB_DEPTH)-1:0] wr_ptr;
-    logic [$clog2(TB_DEPTH)-1:0] tb_ptr;
-    logic [1:0] tb_state;
-    logic [4:0] wr_en;
-    logic stop_write;
-
+    logic [$clog2(TB_DEPTH)-1:0] tb_index_reg;
+    logic [K-2:0] tb_state;
+    logic decision;
+    
+    
+    smu_state_t smu_state;
+    
     // -----------------------------
     // Traceback logic
     // -----------------------------
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-            tb_ptr    <= '0;
+            tb_count <= 1'b0;
             tb_state  <= '0;
             tb_bit    <= 1'b0;
             tb_valid  <= 1'b0;
             tb_active <= 1'b0;
+            tb_index_reg <= K*5;
             wr_ptr <= '0;
-            stop_write <=0;
-        end
-        else if(write_en)begin
-            survivor_mem[wr_ptr] <= { sel_in[3], sel_in[2], sel_in[1], sel_in[0] };
-            wr_ptr <= wr_ptr + 1'b1;
-            if(wr_ptr == TB_DEPTH - 1) begin
-                tb_active <= 1;
-                tb_ptr <= TB_DEPTH - 1;
-                tb_valid <= 1'b0;
-                tb_count <= wr_ptr;
-                tb_state  <= tb_state_start;
-            end
-        end
-        else begin
-            // Perform traceback
-            if (tb_active) begin
-                 // Output the current bit
-                tb_bit   <= survivor_mem[tb_ptr][tb_state];/*
-                case (tb_state)
-                    2'b00: begin
-                        if(survivor_mem[tb_ptr][tb_state] == 0)begin
-                            tb_state <= 2'b00;
-                        end
-                        else tb_state <= 2'b11;
+            smu_state <= START_SMU;
+        end else begin
+            case (smu_state)
+                START_SMU: begin
+                    if(write_en == 1) begin
+                        smu_state <= WRITE;
+                        //wr_ptr = wr_ptr + 1;
+                        tb_count <= TB_DEPTH;
+                        tb_state <= tb_state_start;
+                    end else begin
+                        smu_state <= SMU_IDLE;
                     end
-                    2'b10: begin
-                        if(survivor_mem[tb_ptr][tb_state] == 0)begin
-                            tb_state <= 2'b11;
-                        end
-                        else tb_state <= 2'b00;
-                    end
-                    2'b01: begin
-                        if(survivor_mem[tb_ptr][tb_state] == 0)begin
-                            tb_state <= 2'b10;
-                        end
-                        else tb_state <= 2'b01;
-                    end 
-                    2'b11: begin
-                        if(survivor_mem[tb_ptr][tb_state] == 0)begin
-                            tb_state <= 2'b01;
-                        end
-                        else tb_state <= 2'b10;
-                    end
-                 endcase  */              
-                tb_state <= { survivor_mem[tb_ptr][tb_state], tb_state[1] };// change 1-> 0 for test
-                tb_valid <= 1'b1;
-
-                // Decrement counters
-                tb_ptr   <= tb_ptr - 1'b1;
-                tb_count <= tb_count - 1'b1;
-
-                // Stop traceback when last bit has been output
-                if (tb_ptr == 0 || tb_count == 0) begin
-                    tb_active <= 1'b0;
-                    wr_ptr <= 0;
-                    //stop_write <= 0;
-                   // stops next cycle
                 end
-            end
-         end
-    end
+                WRITE: begin
+                    if(wr_ptr == TB_DEPTH-1) begin
+                       case (K)
+                        7: begin
+                                survivor_mem[wr_ptr] <= { sel_in[63], sel_in[62], sel_in[61], sel_in[60], 
+                                sel_in[59], sel_in[58], sel_in[57], sel_in[56], sel_in[55], sel_in[54], 
+                                sel_in[53], sel_in[52], sel_in[51], sel_in[50], sel_in[49], sel_in[48], sel_in[47], 
+                                sel_in[46], sel_in[45], sel_in[44], sel_in[43], sel_in[42], sel_in[41], sel_in[40], 
+                                sel_in[39], sel_in[38], sel_in[37], sel_in[36], sel_in[35], sel_in[34], sel_in[33], 
+                                sel_in[32], sel_in[31], sel_in[30], sel_in[29], sel_in[28], sel_in[27], sel_in[26], 
+                                sel_in[25], sel_in[24], sel_in[23], sel_in[22], sel_in[21], sel_in[20], sel_in[19], 
+                                sel_in[18], sel_in[17], sel_in[16], sel_in[15], sel_in[14], sel_in[13], sel_in[12], 
+                                sel_in[11], sel_in[10], sel_in[9], sel_in[8], sel_in[7], sel_in[6], sel_in[5], 
+                                sel_in[4], sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end 
+                            5: begin 
+                                survivor_mem[wr_ptr] <= { sel_in[15], sel_in[14], sel_in[13], sel_in[12], 
+                                sel_in[11], sel_in[10], sel_in[9], sel_in[8], sel_in[7], sel_in[6], sel_in[5], 
+                                sel_in[4], sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end
+                            4: begin 
+                                survivor_mem[wr_ptr] <= { sel_in[7], sel_in[6], sel_in[5], sel_in[4], 
+                                sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end
+                            default begin 
+                                survivor_mem[wr_ptr] <= {sel_in[3], sel_in[2], sel_in[1], sel_in[0]};
+                            end
+                            endcase
+                        tb_active <= 1'b1;
+                        smu_state <= TRACEBACK;
+                        tb_valid <= 1'b0;
+                    end else begin
+                        case (K)
+                            7: begin
+                                survivor_mem[wr_ptr] <= { sel_in[63], sel_in[62], sel_in[61], sel_in[60], 
+                                sel_in[59], sel_in[58], sel_in[57], sel_in[56], sel_in[55], sel_in[54], 
+                                sel_in[53], sel_in[52], sel_in[51], sel_in[50], sel_in[49], sel_in[48], sel_in[47], 
+                                sel_in[46], sel_in[45], sel_in[44], sel_in[43], sel_in[42], sel_in[41], sel_in[40], 
+                                sel_in[39], sel_in[38], sel_in[37], sel_in[36], sel_in[35], sel_in[34], sel_in[33], 
+                                sel_in[32], sel_in[31], sel_in[30], sel_in[29], sel_in[28], sel_in[27], sel_in[26], 
+                                sel_in[25], sel_in[24], sel_in[23], sel_in[22], sel_in[21], sel_in[20], sel_in[19], 
+                                sel_in[18], sel_in[17], sel_in[16], sel_in[15], sel_in[14], sel_in[13], sel_in[12], 
+                                sel_in[11], sel_in[10], sel_in[9], sel_in[8], sel_in[7], sel_in[6], sel_in[5], 
+                                sel_in[4], sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end 
+                            5: begin 
+                                survivor_mem[wr_ptr] <= { sel_in[15], sel_in[14], sel_in[13], sel_in[12], 
+                                sel_in[11], sel_in[10], sel_in[9], sel_in[8], sel_in[7], sel_in[6], sel_in[5], 
+                                sel_in[4], sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end
+                            4: begin 
+                                survivor_mem[wr_ptr] <= { sel_in[7], sel_in[6], sel_in[5], sel_in[4], 
+                                sel_in[3], sel_in[2], sel_in[1], sel_in[0] }; 
+                            end
+                            default begin 
+                                survivor_mem[wr_ptr] <= {sel_in[3], sel_in[2], sel_in[1], sel_in[0]};
+                            end
+                         endcase
+                        wr_ptr <= wr_ptr + 1'b1;
+                    end
+                end
+                TRACEBACK: begin
+                    if(tb_start) begin
+                        tb_count <= TB_DEPTH;
+                        tb_state <= tb_state_start;
+                        tb_index_reg <= TB_DEPTH - 1;
+                    end
+                    // Stop traceback when last bit has been output
+                    else if (tb_count == 0) begin
+                        tb_active <= 1'b0;
+                        wr_ptr <= 0;
+                        tb_valid <= 1'b0;
+                        if(write_en == 1) begin
+                            smu_state <= WRITE;
+                            wr_ptr <= wr_ptr + 1;
+                        end else begin
+                            smu_state <= SMU_IDLE;
+                        end
+                    end else begin
+                        decision <= survivor_mem[tb_index_reg][tb_state];
+                        tb_state <= {survivor_mem[tb_index_reg][tb_state], tb_state[K-2:1]};
+                        tb_index_reg <= tb_index_reg - 1;
+                        
+                        tb_bit   <= survivor_mem[tb_index_reg][tb_state];
+                        tb_valid <= 1'b1;
+
+                        // Decrement counters
+                        tb_count <= tb_count - 1'b1;
+                     end
+                end
+                SMU_IDLE: begin
+                    if(write_en == 1) begin
+                        smu_state <= WRITE;
+                        wr_ptr = wr_ptr + 1;
+                        tb_count <= TB_DEPTH;
+                    end else begin
+                        smu_state <= SMU_IDLE;
+                    end
+                
+                end
+             endcase
+          end
+       end
 
 endmodule
