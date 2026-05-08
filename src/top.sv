340059147649f528664a212b4e5fe6075200c891
Jroesner23 <168038057+Jroesner23@users.noreply.github.com>
2026-05-07 22:19:19 -0400

    receive 1024, encode and transmit 2048

--- src/top.sv
+++ src/top.sv
@@ -5,6 +5,7 @@ module top(
     input logic reset,      // reset button/signal
     input logic RxD,        // UART receive line
     input logic btn,
+    input logic en,
     output logic TxD,
     output logic [7:0] LED  // display received byte on LEDs
 );
@@ -15,18 +16,30 @@ logic [1023:0] msg; //input message
 logic [6:0] byte_index;
 
 logic start;
+logic done;
 
 logic busy_prev;
 logic start_prev;
 logic transmit_pulse;
 
-transmitter dut1 (
+//transmitter dut1 (
+//    .clk(clk),
+//    .reset(reset),
+//    .transmit(transmit_pulse),
+//    .data(msg[byte_index*8 +: 8]),
+//    .TxD(TxD),
+//    .busy(busy)
+//);
+
+encode_and_send_FSM #(
+    .DATA_BITS(1024),
+    .K(3)
+) encode_and_send_FSM (
     .clk(clk),
-    .reset(reset),
-    .transmit(transmit_pulse),
-    .data(msg[byte_index*8 +: 8]),
-    .TxD(TxD),
-    .busy(busy)
+    .en(en),
+    .raw_data(msg),
+    .done(done),
+    .txd(TxD)
 );
 
 //transmit V17 (first)
@@ -41,11 +54,11 @@ receiver dut2 (
     .data_valid(data_valid)
 );
 
-debounce dut3 (
-    .pb_1(btn),
-    .clk(clk),
-    .pb_out(start)
-);
+//debounce dut3 (
+//    .pb_1(btn),
+//    .clk(clk),
+//    .pb_out(start)
+//);
 
 //code to take in from a text file. a 1 or a 0. 
 always_ff @(posedge clk or posedge reset) begin
@@ -53,10 +66,10 @@ always_ff @(posedge clk or posedge reset) begin
         msg <= 0;
     end
     else if(data_valid) begin
-        if(rx_data == 8'b00110000)
-            msg <= {msg[1022:0], 1'b0};
-        else if(rx_data == 8'b00110001)
-            msg <= {msg[1022:0], 1'b1};
+        msg <= {msg[1015:0], rx_data};
+    end
+    else begin
+        msg <= msg;
     end
 end
 
@@ -69,27 +82,29 @@ assign LED[5] = byte_index[5];
 assign LED[6] = byte_index[6];
 
 //start at 127th byte and decrement to send all bytes
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        byte_index <= 127;
-        busy_prev <= 0;
-    end
-    else begin
-        busy_prev <= busy;
+//always_ff @(posedge clk or posedge reset) begin
+//    if (reset) begin
+//        byte_index <= 127;
+//        busy_prev <= 0;
+//    end
+//    else begin
+//        busy_prev <= busy;
 
-        // detect falling edge of busy (byte finished)
-        if (busy_prev && !busy) begin
-            if (byte_index == 0)
-                byte_index <= 127;
-            else
-                byte_index <= byte_index - 1;
-        end
-    end
-end
+//        // detect falling edge of busy (byte finished)
+//        if (busy_prev && !busy) begin
+//            if (byte_index == 0)
+//                byte_index <= 127;
+//            else
+//                byte_index <= byte_index - 1;
+//        end
+//    end
+//end
 
-always_ff @(posedge clk) begin
-    start_prev <= start;
-    transmit_pulse <= start && !start_prev; // rising edge detect
-end
+//always_ff @(posedge clk) begin
+//    start_prev <= start;
+//    transmit_pulse <= start && !start_prev; // rising edge detect
+//end
 
 endmodule
+
+
